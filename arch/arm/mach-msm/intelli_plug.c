3be7ae5742902fab31607ffef9c0d75a25b60030
BrateloSlava <d_slava@list.ru>
2014-03-20 08:41:52 +0200

    intelli_plug: enable by default

--- arch/arm/mach-msm/intelli_plug.c
+++ arch/arm/mach-msm/intelli_plug.c
@@ -54,7 +54,7 @@ static struct delayed_work intelli_plug_boost;
 static struct workqueue_struct *intelliplug_wq;
 static struct workqueue_struct *intelliplug_boost_wq;
 
-static unsigned int intelli_plug_active = 0;
+static unsigned int intelli_plug_active = 1;
 module_param(intelli_plug_active, uint, 0644);
 
 #ifndef CONFIG_INTELLI_PLUG_DUAL
