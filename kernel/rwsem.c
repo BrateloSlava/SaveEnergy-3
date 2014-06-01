1a6df15cab77a4824f686d39655740f8af956e77
BrateloSlava <d_slava@list.ru>
2014-06-01 13:15:15 +0300

    lockdep, rwsem: provide down_write_nest_lock()
    down_write_nest_lock() provides a means to annotate locking
    scenario
    where an outer lock is guaranteed to serialize the order nested
    locks
    are being acquired.
    
    This is analogoue to already existing
    mutex_lock_nest_lock() and
    spin_lock_nest_lock().
    
    Change-Id:
    Ib6124fd59cfc01c225cdb6fa46eec2b6d57f8c18
    Signed-off-by: Jiri Kosina
    <jkosina@suse.cz>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Ingo Molnar
    <mingo@kernel.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Mel
    Gorman <mel@csn.ul.ie>
    Tested-by: Sedat Dilek
    <sedat.dilek@gmail.com>
    Signed-off-by: Andrew Morton
    <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds
    <torvalds@linux-foundation.org>
    
    idea is taken here -
    https://github.com/jrior001/android_kernel_samsung_d2/commit/310562f1e3619115cba19db86b59387905967052

--- kernel/rwsem.c
+++ kernel/rwsem.c
@@ -12,6 +12,9 @@
 
 #include <linux/atomic.h>
 
+/*
+ * lock for reading
+ */
 void __sched down_read(struct rw_semaphore *sem)
 {
 	might_sleep();
@@ -22,6 +25,9 @@ void __sched down_read(struct rw_semaphore *sem)
 
 EXPORT_SYMBOL(down_read);
 
+/*
+ * trylock for reading -- returns 1 if successful, 0 if contention
+ */
 int down_read_trylock(struct rw_semaphore *sem)
 {
 	int ret = __down_read_trylock(sem);
@@ -33,6 +39,9 @@ int down_read_trylock(struct rw_semaphore *sem)
 
 EXPORT_SYMBOL(down_read_trylock);
 
+/*
+ * lock for writing
+ */
 void __sched down_write(struct rw_semaphore *sem)
 {
 	might_sleep();
@@ -43,6 +52,9 @@ void __sched down_write(struct rw_semaphore *sem)
 
 EXPORT_SYMBOL(down_write);
 
+/*
+ * trylock for writing -- returns 1 if successful, 0 if contention
+ */
 int down_write_trylock(struct rw_semaphore *sem)
 {
 	int ret = __down_write_trylock(sem);
@@ -54,6 +66,9 @@ int down_write_trylock(struct rw_semaphore *sem)
 
 EXPORT_SYMBOL(down_write_trylock);
 
+/*
+ * release a read lock
+ */
 void up_read(struct rw_semaphore *sem)
 {
 	rwsem_release(&sem->dep_map, 1, _RET_IP_);
@@ -63,6 +78,9 @@ void up_read(struct rw_semaphore *sem)
 
 EXPORT_SYMBOL(up_read);
 
+/*
+ * release a write lock
+ */
 void up_write(struct rw_semaphore *sem)
 {
 	rwsem_release(&sem->dep_map, 1, _RET_IP_);
@@ -72,8 +90,15 @@ void up_write(struct rw_semaphore *sem)
 
 EXPORT_SYMBOL(up_write);
 
+/*
+ * downgrade write lock to read lock
+ */
 void downgrade_write(struct rw_semaphore *sem)
 {
+	/*
+	 * lockdep: a downgraded write will live on as a write
+	 * dependency.
+	 */
 	__downgrade_write(sem);
 }
 
@@ -90,6 +115,16 @@ void down_read_nested(struct rw_semaphore *sem, int subclass)
 }
 
 EXPORT_SYMBOL(down_read_nested);
+
+void _down_write_nest_lock(struct rw_semaphore *sem, struct lockdep_map *nest)
+{
+	might_sleep();
+	rwsem_acquire_nest(&sem->dep_map, 0, 0, nest, _RET_IP_);
+
+	LOCK_CONTENDED(sem, __down_write_trylock, __down_write);
+}
+
+EXPORT_SYMBOL(_down_write_nest_lock);
 
 void down_write_nested(struct rw_semaphore *sem, int subclass)
 {
